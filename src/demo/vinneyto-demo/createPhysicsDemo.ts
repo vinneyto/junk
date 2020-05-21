afafe9ea82d53eac76da758bfec7879274e626c5
vinneyto <vinneyto@gmail.com>
2020-05-22 02:33:10 +0300

    adds nphysics demo with cuboids

--- src/demo/vinneyto-demo/createPhysicsDemo.ts
+++ src/demo/vinneyto-demo/createPhysicsDemo.ts
@@ -1,23 +1,95 @@
 import { Demo } from '../Demo';
+import { createRenderer, resizeRenderer } from '../../util';
+import {
+  PerspectiveCamera,
+  Scene,
+  PlaneGeometry,
+  Mesh,
+  DirectionalLight,
+  AxesHelper,
+  MeshBasicMaterial,
+  Object3D,
+  BoxGeometry,
+  MeshPhongMaterial,
+  Matrix4,
+  DoubleSide,
+  AmbientLight,
+} from 'three';
+import { CameraController } from '../../CameraController';
 
 export async function createPhysicsDemo(): Promise<Demo> {
   const { PhysicsDemo } = await import('../../../wasm/pkg');
 
   const demo = new PhysicsDemo();
+  const renderer = createRenderer();
+  const camera = new PerspectiveCamera(75, 1, 0.01, 20);
+  const cameraController = new CameraController(7, 0.01);
+  cameraController.setRotation(Math.PI / 6, Math.PI / 8);
+  const scene = new Scene();
 
-  const amount = demo.get_amount();
+  const geometry = new PlaneGeometry(10, 10);
+  geometry.rotateX(-Math.PI / 2);
+  const material = new MeshBasicMaterial({
+    color: 'gray',
+    side: DoubleSide,
+  });
+  const box = new Mesh(geometry, material);
+  scene.add(box);
 
-  for (let i = 0; i < amount; i++) {
-    const array = demo.get_object_data(i);
+  const light = new DirectionalLight();
+  light.position.set(0, 1, 0);
+  scene.add(light);
 
-    if (i === 2) {
-      array[3] = 5;
+  scene.add(new AmbientLight());
+
+  scene.add(new AxesHelper(1));
+
+  const gameObjects: Object3D[] = [];
+  const transformMatrix = new Matrix4();
+
+  const render = () => {
+    resizeRenderer(renderer, camera);
+
+    demo.step();
+
+    const amount = demo.get_amount();
+
+    for (let i = 0; i < gameObjects.length; i++) {
+      gameObjects[i].visible = false;
     }
 
-    console.log(array[0], array[1], array[2], array[3]);
-  }
+    for (let i = 0; i < amount; i++) {
+      const render_data = demo.get_render_data(i);
+      transformMatrix.fromArray(render_data, 0);
 
-  const render = () => {};
+      if (gameObjects.length < i + 1) {
+        const cuboid = createCuboid();
+        gameObjects.push(cuboid);
+        scene.add(cuboid);
+        console.log('add cuboid');
+      }
+
+      const object = gameObjects[i];
+      transformMatrix.decompose(
+        object.position,
+        object.quaternion,
+        object.scale
+      );
+
+      object.scale.fromArray(render_data, 16);
+      object.visible = true;
+    }
+
+    cameraController.update(camera);
+
+    renderer.render(scene, camera);
+  };
 
   return { render };
+}
+
+function createCuboid() {
+  const g = new BoxGeometry(1, 1, 1);
+  const m = new MeshPhongMaterial({ color: 'green' });
+  return new Mesh(g, m);
 }
--- wasm/src/demo/physics.rs
+++ wasm/src/demo/physics.rs
@@ -1,8 +1,12 @@
 use js_sys::Float32Array;
 use na::Vector3;
+use ncollide3d::shape::{Cuboid, ShapeHandle};
 use nphysics3d::force_generator::DefaultForceGeneratorSet;
 use nphysics3d::joint::DefaultJointConstraintSet;
-use nphysics3d::object::{DefaultBodySet, DefaultColliderSet};
+use nphysics3d::object::{
+  BodyPartHandle, ColliderDesc, DefaultBodyHandle, DefaultBodySet, DefaultColliderSet, Ground,
+  RigidBodyDesc,
+};
 use nphysics3d::world::{DefaultGeometricalWorld, DefaultMechanicalWorld};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -12,7 +16,8 @@ use crate::renderer::webgl::context::get_typed_array_from_slice;
 #[wasm_bindgen]
 pub struct PhysicsDemo {
   render_data: Vec<f32>,
-  positions: Vec<Vector3<f32>>,
+  game_objects: Vec<Box<dyn GameObject>>,
+
   mechanical_world: DefaultMechanicalWorld<f32>,
   geometrical_world: DefaultGeometricalWorld<f32>,
   bodies: DefaultBodySet<f32>,
@@ -26,19 +31,63 @@ impl PhysicsDemo {
   #[wasm_bindgen(constructor)]
   pub fn new() -> PhysicsDemo {
     let render_data = vec![0.0; 100];
-    let positions = vec![Vector3::new(0.0, 0.0, 0.0); 5];
+    let mut game_objects: Vec<Box<dyn GameObject>> = vec![];
 
     let mechanical_world = DefaultMechanicalWorld::new(Vector3::new(0.0, -9.81, 0.0));
     let geometrical_world = DefaultGeometricalWorld::new();
 
-    let bodies = DefaultBodySet::new();
-    let colliders = DefaultColliderSet::new();
+    let mut bodies = DefaultBodySet::new();
+    let mut colliders = DefaultColliderSet::new();
     let joint_constraints = DefaultJointConstraintSet::new();
     let force_generators = DefaultForceGeneratorSet::new();
 
+    let ground_thickness = 0.2;
+    let ground_shape = ShapeHandle::new(Cuboid::new(Vector3::new(5.0, ground_thickness, 5.0)));
+
+    let ground_handle = bodies.insert(Ground::new());
+    let co = ColliderDesc::new(ground_shape)
+      .translation(Vector3::y() * -ground_thickness)
+      .build(BodyPartHandle(ground_handle, 0));
+    colliders.insert(co);
+
+    game_objects.push(Box::new(CuboidObject::new(
+      &mut bodies,
+      &mut colliders,
+      Vector3::new(0.5, 0.5, 0.5),
+      Vector3::new(0.0, 2.0, 0.0),
+    )));
+
+    game_objects.push(Box::new(CuboidObject::new(
+      &mut bodies,
+      &mut colliders,
+      Vector3::new(0.5, 0.5, 0.5),
+      Vector3::new(0.7, 4.0, 0.0),
+    )));
+
+    game_objects.push(Box::new(CuboidObject::new(
+      &mut bodies,
+      &mut colliders,
+      Vector3::new(0.5, 0.5, 0.5),
+      Vector3::new(-0.7, 4.0, 0.0),
+    )));
+
+    game_objects.push(Box::new(CuboidObject::new(
+      &mut bodies,
+      &mut colliders,
+      Vector3::new(0.3, 0.5, 0.7),
+      Vector3::new(-0.7, 6.0, -1.0),
+    )));
+
+    game_objects.push(Box::new(CuboidObject::new(
+      &mut bodies,
+      &mut colliders,
+      Vector3::new(0.3, 0.5, 0.7),
+      Vector3::new(-0.2, 8.0, 1.0),
+    )));
+
     PhysicsDemo {
       render_data,
-      positions,
+      game_objects,
       mechanical_world,
       geometrical_world,
       bodies,
@@ -59,11 +108,11 @@ impl PhysicsDemo {
   }
 
   pub fn get_amount(&self) -> u32 {
-    self.positions.len() as u32
+    self.game_objects.len() as u32
   }
 
-  pub fn get_object_data(&mut self, index: usize) -> Float32Array {
-    update_render_data(&mut self.render_data, &self.positions[index]);
+  pub fn get_render_data(&mut self, index: usize) -> Float32Array {
+    self.game_objects[index].update_render_data(&self.colliders, &mut self.render_data);
 
     get_typed_array_from_slice(&self.render_data)
       .dyn_into::<Float32Array>()
@@ -71,8 +120,47 @@ impl PhysicsDemo {
   }
 }
 
-fn update_render_data(data: &mut [f32], position: &Vector3<f32>) {
-  data[0] = position.x;
-  data[1] = position.y;
-  data[2] = position.z;
+pub trait GameObject {
+  fn update_render_data(&self, colliders: &DefaultColliderSet<f32>, data: &mut [f32]);
+}
+
+pub struct CuboidObject {
+  handle: DefaultBodyHandle,
+  half_size: Vector3<f32>,
+}
+
+impl CuboidObject {
+  pub fn new(
+    bodies: &mut DefaultBodySet<f32>,
+    colliders: &mut DefaultColliderSet<f32>,
+    half_size: Vector3<f32>,
+    translation: Vector3<f32>,
+  ) -> Self {
+    let body = RigidBodyDesc::new().translation(translation).build();
+
+    let handle = bodies.insert(body);
+
+    let cuboid = ShapeHandle::new(Cuboid::new(half_size));
+
+    let co = ColliderDesc::new(cuboid)
+      .density(1.0)
+      .build(BodyPartHandle(handle, 0));
+    colliders.insert(co);
+
+    CuboidObject { handle, half_size }
+  }
+}
+
+impl GameObject for CuboidObject {
+  fn update_render_data(&self, bodies: &DefaultColliderSet<f32>, data: &mut [f32]) {
+    if let Some(collider) = bodies.get(self.handle) {
+      let m = collider.position().to_homogeneous();
+      for i in 0..16 {
+        data[i] = m[i];
+      }
+      data[16] = self.half_size.x * 2.0;
+      data[17] = self.half_size.y * 2.0;
+      data[18] = self.half_size.z * 2.0;
+    }
+  }
 }
