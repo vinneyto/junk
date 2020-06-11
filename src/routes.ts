825d8bb46913df174e7d7f8d7ead3c2e24e9070c
natalyj <natalyj100@gmail.com>
2020-06-11 18:41:22 +0300

    reworked main page styles

--- src/routes.ts
+++ src/routes.ts
@@ -8,19 +8,68 @@ import { createPhysicsDemo } from './demo/vinneyto-demo/createPhysicsDemo';
 import { drawTriangle as natalyjDrawTriangle } from './demo/natalyj-demo/Triangle/drawTriangle';
 import { createCameraDemo as vinneytoCreateCameraDemo } from './demo/vinneyto-demo/createCameraDemo';
 
+interface RouteInfo {
+  demo: () => Promise<Demo>;
+  title: string;
+  authors: string;
+  tags?: string[];
+}
+
 const vinneytoWasmTriangleDemo = createWasmDemoFactory('TriangleDemo');
 const vinneytoWasmSkinningDemo = createWasmDemoFactory('SkinningDemo');
 
-export const routes = new Map<string, () => Promise<Demo>>();
-routes.set('box', createBoxDemo);
-routes.set(
-  'natalyj-singleColorShaderMaterial',
-  natalyjSingleColorShaderMaterial
-);
-routes.set('natalyj-splitColorShaderMaterial', natalyjSplitColorShaderMaterial);
-routes.set('vinneyto-wasm-webgl2-triangle', vinneytoWasmTriangleDemo);
-routes.set('vinneyto-wasm-skinning', vinneytoWasmSkinningDemo);
-routes.set('natalyj-scanner', natalyjScanner);
-routes.set('vinneyto-physics', createPhysicsDemo);
-routes.set('natalyj-triangle', natalyjDrawTriangle);
-routes.set('vinneyto-camera-demo', vinneytoCreateCameraDemo);
+export const routes = new Map<string, RouteInfo>();
+routes.set('box', {
+  demo: createBoxDemo,
+  title: 'Box',
+  authors: 'vinneyto',
+  tags: ['threejs', 'typescript'],
+});
+routes.set('natalyj-singleColorShaderMaterial', {
+  demo: natalyjSingleColorShaderMaterial,
+  title: 'Single Color Shader Material',
+  authors: 'natalyj',
+  tags: ['threejs', 'typescript'],
+});
+routes.set('natalyj-splitColorShaderMaterial', {
+  demo: natalyjSplitColorShaderMaterial,
+  title: 'Split Color Shader Material',
+  authors: 'natalyj, vinneyto',
+  tags: ['threejs', 'typescript'],
+});
+routes.set('vinneyto-wasm-webgl2-triangle', {
+  demo: vinneytoWasmTriangleDemo,
+  title: 'WebGL2 Triangle',
+  authors: 'vinneyto',
+  tags: ['wasm', 'rust', 'webgl2'],
+});
+routes.set('vinneyto-wasm-skinning', {
+  demo: vinneytoWasmSkinningDemo,
+  title: 'Skinning',
+  authors: 'vinneyto',
+  tags: ['wasm', 'rust'],
+});
+routes.set('natalyj-scanner', {
+  demo: natalyjScanner,
+  title: 'Scanner',
+  authors: 'natalyj, vinneyto',
+  tags: ['threejs', 'typescript'],
+});
+routes.set('vinneyto-physics', {
+  demo: createPhysicsDemo,
+  title: 'Physics',
+  authors: 'vinneyto',
+  tags: ['wasm', 'rust', 'typescript'],
+});
+routes.set('natalyj-triangle', {
+  demo: natalyjDrawTriangle,
+  title: 'Triangle',
+  authors: 'natalyj',
+  tags: ['typescript'],
+});
+routes.set('vinneyto-camera-demo', {
+  demo: vinneytoCreateCameraDemo,
+  title: 'Camera',
+  authors: 'vinneyto',
+  tags: ['threejs', 'typescript'],
+});
