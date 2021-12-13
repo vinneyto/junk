1ae0a36d97b41c0c26ee46acf0f52c926d59ab44
lvinnichenko <lvinnichenko@aligntech.com>
2021-12-13 19:14:08 +0300

    adds titanic training demo

--- src/demo/vinneyto-demo/titanicTraining.ts
+++ src/demo/vinneyto-demo/titanicTraining.ts
@@ -13,11 +13,56 @@ export async function titanicTraining() {
   const testX = tf.tensor2d(titanicTestX.data);
   const testY = tf.tensor1d(titanicTestY.data);
 
-  console.log(trainX.shape);
-  console.log(trainY.shape);
+  const model = tf.sequential();
 
-  console.log(testX.shape);
-  console.log(testY.shape);
+  model.add(
+    tf.layers.dense({
+      inputShape: [trainX.shape[1]],
+      units: 120,
+      activation: 'relu',
+      kernelInitializer: 'heNormal',
+    })
+  );
+
+  model.add(
+    tf.layers.dense({
+      units: 64,
+      activation: 'relu',
+    })
+  );
+
+  model.add(
+    tf.layers.dense({
+      units: 32,
+      activation: 'relu',
+    })
+  );
+
+  model.add(
+    tf.layers.dense({
+      units: 1,
+      activation: 'sigmoid',
+    })
+  );
+
+  model.compile({
+    optimizer: 'adam',
+    loss: 'binaryCrossentropy',
+    metrics: ['accuracy'],
+  });
+
+  await model.fit(trainX, trainY, {
+    batchSize: 32,
+    epochs: 100,
+    validationData: [testX, testY],
+    callbacks: {
+      onEpochEnd: (epoch, log) => {
+        console.log(epoch, log);
+      },
+    },
+  });
 
   tf.dispose([trainX, trainY, testX, testY]);
+
+  model.dispose();
 }
