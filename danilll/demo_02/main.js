42803dbb9927cc9120388cf0cd411912f2a2a872
lvinnichenko <lvinnichenko@aligntech.com>
2022-09-18 22:41:54 +0200

    prepare demo2 for dan

---
+++ danilll/demo_02/main.js
@@ -0,0 +1,10 @@
+let counter = 0; // переменная счетчик
+
+// найти кнопку button
+// найти span counter
+
+function onButtonClick() {
+  // увеличить counter на единицу
+}
+
+// подписаться на клик кнопки
